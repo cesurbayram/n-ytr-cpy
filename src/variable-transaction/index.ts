5667f5a4d76f59e29a9d84bf3ca9ea30d0ed5765
Cesur Bayram <cesr.bayram@gmail.com>
2024-10-14 10:05:34 +0300

    fixed code block for variables

--- src/variable-transaction/index.ts
+++ src/variable-transaction/index.ts
@@ -7,19 +7,16 @@ interface Message {
   robot_ip: string; 
   value: number;
   name: string;
-  no: number; // no değerini de ekledik
+  no: number; 
 }
 
 const variableTransaction = async (message: Message): Promise<void> => {
   try {
     let updateQuery = '';
     let controllerId = '';
-    
     // Gelen mesaja göre ilgili tabloya kaydet
     switch (message.type) {
       case 'd_read':
-
-
         updateQuery =
           `UPDATE d_read SET value = $2, name = $3, no = $4 WHERE controller_id = $1`;
         
@@ -27,33 +24,27 @@ const variableTransaction = async (message: Message): Promise<void> => {
       case 'r_read':
         updateQuery =
           `UPDATE r_read SET value = $2, name = $3, no = $4 WHERE controller_id = $1 `;
-        
         break;
       case 's_read':
         updateQuery =
           `UPDATE s_read SET value = $2, name = $3, no = $4 WHERE controller_id = $1 `;
-        
         break;
       case 'i_read':
         updateQuery =
           `UPDATE i_read SET value = $2, name = $3, no = $4 WHERE controller_id = $1 `;
-        
         break;
       case 'b_read':
         updateQuery =
           `UPDATE b_read SET value = $2, name = $3, no = $4 WHERE controller_id = $1 `;
-        
         break;
       default:
         console.log('Unknown variable type:', message.type);
         return;
     }
-
     const controllerDbRes = await dbPool.query(`SELECT id FROM controller WHERE ip_address = $1`, [message.robot_ip])
     if(controllerDbRes.rowCount && controllerDbRes.rowCount > 0) {
       controllerId = controllerDbRes.rows[0]?.id
     }
-    
     // Önce güncellemeyi dene
     await dbPool.query(updateQuery, [controllerId, message.value, message.name, message.no]);    
   } catch (err) {
